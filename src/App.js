0261fbed1ba2aa88dbb972188827cb28f5af601a
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-15 20:55:26 +0100

    added a click function to filter and remove clicked array items from the DOM, using setEvents to return the ids NOT clicked

--- src/App.js
+++ src/App.js
@@ -5,25 +5,28 @@ import { useState } from 'react'
 function App() {
   // name is the current default name; 
   // setName is to set a function to set the name to a different one
-  const [name, setName] = useState('mario');
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1 },
     {title: "bowser's live stream", id: 2 },
     {title: "race on moo moo farm", id: 3}
   ])
 
-  const handelClick = () => {
-    setName('luigi')
-    console.log(name)
+  const handelClick = (id) => {
+    setEvents ((previousEvents) => {
+        return previousEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id);
+    
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handelClick}>Change name</button>
       {events.map( (event, index) => (
         <div key={event.id}>
           <h2>{index + 1}: {event.title}</h2>
+          <button onClick={() => handelClick(event.id)}>delete event</button>
         </div>
       ))}
     </div>
