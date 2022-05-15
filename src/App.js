ca2412a3c8b6db29661c090d44b3ec4d78da02c0
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-15 19:46:11 +0100

    ouput a list of data objects using the map( ) method

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ function App() {
   // name is the current default name; 
   // setName is to set a function to set the name to a different one
   const [name, setName] = useState('mario');
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1 },
+    {title: "bowser's live stream", id: 2 },
+    {title: "race on moo moo farm", id: 3}
+  ])
 
   const handelClick = () => {
     setName('luigi')
@@ -16,6 +21,11 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handelClick}>Change name</button>
+      {events.map( (event, index) => (
+        <div key={event.id}>
+          <h2>{index + 1}: {event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
