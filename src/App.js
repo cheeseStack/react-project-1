c9a7879836377a319176d7dc119359925a5c8619
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-15 18:47:15 +0100

    deleted default App.js template and replaced with a button to show need for useState to change variables

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let name = 'mario';
+
+  const handelClick = () => {
+    name = 'luigi';
+    console.log(name);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <button onClick={handelClick}>Change name</button>
     </div>
   );
 }
