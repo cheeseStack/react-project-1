c54fa671155b9085d2c50fef11fad667534cd80a
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-15 22:00:36 +0100

    added hide and show buttons using Boolean true or false and logical AND && to show or hide the JSX template

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  // name is the current default name; 
+  // //s.22: conditional Templates: show based on true or false
+  const [showEvents, setShowEvents] = useState(true)
   // setName is to set a function to set the name to a different one
   const [events, setEvents] = useState([
     {title: "mario's birthday bash", id: 1 },
     {title: "bowser's live stream", id: 2 },
     {title: "race on moo moo farm", id: 3}
   ])
+
+  console.log(showEvents);
 
   const handelClick = (id) => {
     setEvents ((previousEvents) => {
@@ -18,12 +21,26 @@ function App() {
       })
     })
     console.log(id);
-    
   }
 
   return (
     <div className="App">
-      {events.map( (event, index) => (
+      {/* below: s22: https://netninja.dev/courses/1546635/lectures/35757442 
+      Button to show or hide the template below, showing the object ddata  */}
+      {showEvents && (
+        <div>
+          <button onClick ={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      
+      {!showEvents && (
+        <div>
+          <button onClick ={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+
+      {/* use logical AND && to sow or hide the template, only showing the RHS of the code if the LHS is true */}
+      {showEvents && events.map( (event, index) => (
         <div key={event.id}>
           <h2>{index + 1}: {event.title}</h2>
           <button onClick={() => handelClick(event.id)}>delete event</button>
