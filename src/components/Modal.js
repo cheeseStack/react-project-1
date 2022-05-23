39a90c6a9beec03b37a381c8f0fc36f554090164
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-23 22:19:12 +0100

    added inline styles to the modal tag in the Modal.js file

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -4,13 +4,17 @@ import './Modal.css'
 
 export default function Modal(props) {
   return ReactDOM.createPortal ( (
-    <div className='modal-backdrop'>
-        <div className="modal">
-            {props.children}
-            <button onClick={props.handleClose}>close</button>
+    <div className="modal-page">
+        <div className='modal-backdrop'>
+            <div className="modal" style={{
+                border: "4px solid",
+                borderColor: "#ff4500",
+                textAlign: "center"
+            }}>
+                {props.children}
+                <button onClick={props.handleClose}>close</button>
+            </div>
         </div>
-
-
     </div>
   ), document.body )
 }
