9e33fa4f6bbe52b923158f4f299039e5be970193
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-24 09:34:51 +0100

    added .module css for EventList.js to put events on cards

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import styles from './EventList.module.css'
 
 export default function EventList({events, handelClick}) { // this destructures rather than use props keyword
   return (
     <div>
         {events.map( (event, index) => (
-        <React.Fragment key={event.id}>
+        <div className={styles.card} key={event.id}>
           <h2>{index + 1}: {event.title}</h2>
           <button onClick={() => handelClick(event.id)}>delete event</button>
-        </React.Fragment>
+        </div>
       ))}
     </div>
   )
