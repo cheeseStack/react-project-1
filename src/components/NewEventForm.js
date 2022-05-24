a494415aaca2b49ddbf735c8107c7b08c8c97605
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-24 10:51:55 +0100

    added a clear form function and set the value of the inputds to the original useState of an empty string - a controlled input

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -6,6 +6,11 @@ export default function NewEventForm() {
     const [title, setTitle] = useState('')
     const [date, setDate] = useState(' ')
 
+    const resetForm = () => {
+        setTitle('')
+        setDate('')
+    }
+
     // const handleChange = (e) => {
     //     setTitle(e.target.value)
     // }
@@ -16,16 +21,25 @@ export default function NewEventForm() {
     <form className="new-event-form">
         <label> 
             <span>Event Title: </span>
-            <input type="text" onChange={(e) => setTitle(e.target.value)} />
+            <input 
+            type="text" 
+            onChange={(e) => setTitle(e.target.value)} 
+            value={title}
+            />
         </label>
 
         <label>
             <span>Event Date: </span>
-            <input type="date" onChange={(e) => setDate(e.target.value)}/>
+            <input 
+            type="date" 
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+            />
         </label>
         <button>Submit</button>
         <p>Title: {title}</p>
         <p>Date: {date}</p>
+        <p onClick={resetForm}>reset the form </p>
     </form>
   )
 }
