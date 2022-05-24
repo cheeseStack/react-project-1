557f737ff73a8c6358a1144019615442643471cc
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-24 10:41:17 +0100

    added useState for title and date, and added an onChange inline function to change to e.target.value

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,20 +1,31 @@
 import './NewEventForm.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 export default function NewEventForm() {
+    const [title, setTitle] = useState('')
+    const [date, setDate] = useState(' ')
+
+    // const handleChange = (e) => {
+    //     setTitle(e.target.value)
+    // }
+    // instead of the above, use inline functions directly in the onChange function
+
+
   return (
     <form className="new-event-form">
         <label> 
             <span>Event Title: </span>
-            <input type="text" />
+            <input type="text" onChange={(e) => setTitle(e.target.value)} />
         </label>
 
         <label>
             <span>Event Date: </span>
-            <input type="date" />
+            <input type="date" onChange={(e) => setDate(e.target.value)}/>
         </label>
         <button>Submit</button>
+        <p>Title: {title}</p>
+        <p>Date: {date}</p>
     </form>
   )
 }
