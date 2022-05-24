e9f2a552b6bb4b9567e2bb2ce6ef4715d0c1d6ee
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-05-24 09:22:28 +0100

    added conditional css class to the modal button: bold when isSalesModal={true}

--- src/App.js
+++ src/App.js
@@ -1,83 +1,56 @@
-// import logo from './logo.svg';
-import './App.css';
+import './App.css'
 import React, { useState } from 'react'
 import Title from './components/Title'
 import Modal from './components/Modal'
-import EventList from './components/EventList';
+import EventList from './components/EventList'
 
 function App() {
-  // //s.22: conditional Templates: show based on true or false
+  const [showModal, setShowModal] = useState(false)
   const [showEvents, setShowEvents] = useState(true)
-  // setName is to set a function to set the name to a different one
   const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1 },
-    {title: "bowser's live stream", id: 2 },
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
     {title: "race on moo moo farm", id: 3}
   ])
 
-  // below: Modal useState 
-  const [showModal, setShowModal] = useState(false);
-
-  console.log(showModal);
-
-  const handelClick = (id) => {
-    setEvents ((previousEvents) => {
-        return previousEvents.filter((event) => {
-        return id !== event.id
-      })
+  const handleClick = (id) => {
+    setEvents(prevEvents => {
+      return prevEvents.filter(event => id !== event.id)
     })
-    console.log(id);
   }
 
-  // below: modal handleClose function
   const handleClose = () => {
     setShowModal(false)
   }
 
-  // below: open the modal with a button:
-  // const handleOpen = () => {
-  //   setShowModal(true)
-  // } 
-
   const subtitle = "All the latest events in Marioland"
-  const subtitle2 = "Another subtitle"
 
   return (
     <div className="App">
-    
-      <Title title="Events in Your Area" subtitle={subtitle}/>
-      {/* <Title title="another title" subtitle={subtitle2}/> */}
+      <Title title="Marioland Events" subtitle={subtitle} />
+      
       {showEvents && (
         <div>
-          <button onClick ={() => setShowEvents(false)}>hide events</button>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
         </div>
       )}
-      
       {!showEvents && (
         <div>
-          <button onClick ={() => setShowEvents(true)}>show events</button>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
+      
+      {showModal && (
+        <Modal handleClose={handleClose} isSalesModal={true}>
+          <h2>Terms and Conditions</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error odit nam et reprehenderit quibusdam temporibus officia dolorum quo sint nemo quis, laborum, quasi nisi fugit praesentium debitis repudiandae! Sapiente, omnis.</p>
+        </Modal>
+      )}
 
-      {/* use logical AND && to sow or hide the template, only showing the RHS of the code if the LHS is true */}
-      {showEvents && <EventList events={events} handelClick={handelClick}/> }
-
-    {/* Modal from part 28 goes below */}
-    {/* <Modal> 
-      <h2>10% Off Coupon Code!!</h2>
-      <p>Use the code NINJA10 at the checkout</p>
-    </Modal> */}
-
-    {showModal && <Modal handleClose={handleClose} isSalesModal={false}> 
-      <h2>Terms and Conditions</h2>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, eum. Incidunt necessitatibus saepe, nihil architecto quis labore corporis omnis alias expedita ipsam corrupti cupiditate, doloribus ex aliquid minima numquam! Eaque!</p>
-      <a href="#">find out more . . .</a>
-    </Modal>}
-
-    <div>
-      <button onClick={() => setShowModal(true)}>Show your special offer</button>
-    </div>
-
+      <div>
+        <button onClick={() => setShowModal(true)}>Show Modal</button>
+      </div>
     </div>
   );
 }
